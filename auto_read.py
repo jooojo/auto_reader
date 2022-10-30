2cd02463e138c8808416f684f64ca87d36db10f3
jojolno <jojolno@outlook.com>
2022-10-30 23:16:35 +0800

    initial

---
+++ auto_read.py
@@ -0,0 +1,62 @@
+#!/bin/env python
+import pandas
+import torch
+import typer
+from datasets import Dataset
+from rich.progress import Progress, SpinnerColumn, TextColumn, track
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+from transformers.pipelines.pt_utils import KeyDataset
+
+
+def initial_pipeline(model_name):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    return pipeline(task="text2text-generation",
+                    model=model,
+                    tokenizer=tokenizer,
+                    device=device)
+
+
+app = typer.Typer()
+
+
+@app.command()
+def answer(
+    question: str = typer.Argument(
+        ...,
+        help=
+        "The question to answer"
+    ),
+    papers: str = typer.Argument(
+        ...,
+        help=
+        "The tsv file of papers infomation with columns `title` and `abstract`"
+    ),
+    outfile: str = typer.Argument(...),
+    model: str = typer.Option("google/flan-t5-base"),
+    batch_size: int = typer.Option(64, "--batch_size", "-bs")):
+    with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            transient=False,
+    ) as progress:
+        progress.add_task(description="Loading Model...", total=None)
+        pipe = initial_pipeline(model)
+    df = pandas.read_csv(papers, sep='\t')
+    inputs = df.apply(lambda x: f"{x['title']}\n{x['abstract']}\n\n{question}",
+                      axis=1)
+    data = KeyDataset(Dataset.from_pandas(inputs.to_frame()), key='0')
+    answers = [
+        output[0]['generated_text']
+        for output in track(pipe(data, batch_size=batch_size),
+                            total=inputs.size,
+                            description="Reading...")
+    ]
+    with open(outfile, 'w') as fout:
+        fout.write(f"{question}\n")
+        fout.write("\n".join(answers))
+
+
+if __name__ == "__main__":
+    app()
---
+++ scripts/paper_crawler.py
@@ -0,0 +1,73 @@
+import urllib.parse
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+# parse paper info from paper url
+def parse_paper_info(url):
+    with urlopen(url) as f:
+        soup = BeautifulSoup(f.read(), "html.parser")
+        dl_data = soup.find_all("dl")
+        title = soup.find('div', id='papertitle').get_text().strip()
+        authors = soup.find('div', id='authors').get_text().strip()
+        abstract = soup.find('div', id='abstract').get_text().strip()
+        link = urllib.parse.urljoin(url, soup.find('a', text='pdf')['href'])
+        return '\t'.join([title, authors, link, abstract])
+
+# parse paper lists from conference index page
+def parse_daily_url(url):
+    with urlopen(url) as f:
+        soup = BeautifulSoup(f.read(), "html.parser")
+        dl_data = soup.find_all("dl")
+        data = dl_data[0]
+        for dt in data.find_all("dt"):
+            paper_link = urllib.parse.urljoin(url, dt.find('a')['href'])
+            yield paper_link
+
+def get_cvf_paper_list(index_url):
+    with urlopen(index_url) as f:
+        soup = BeautifulSoup(f.read(), "html.parser")
+        dl_data = soup.find("dl")
+        for dd in dl_data.find_all("dd"):
+           daily_url = urllib.parse.urljoin(index_url, dd.find('a')['href'])
+           if 'all' in daily_url:
+               continue
+           for paper in parse_daily_url(daily_url):
+               yield paper
+
+# parse paper lists from conference index page
+def parse_eccv(url, t=0):
+    with urlopen(url) as f:
+        soup = BeautifulSoup(f.read(), "html.parser")
+        dl_data = soup.find_all("dl")
+        data = dl_data[t]
+        for dt in tqdm(data.find_all("dt")):
+            paper_link = urllib.parse.urljoin(url, dt.find('a')['href'])
+            yield paper_link
+
+eccv_url = 'http://www.ecva.net/papers.php'
+conference_list = parse_eccv(eccv_url, 0)
+with open(f'./data/ECCV2022.tsv', 'w') as outfile:
+    outfile.write("title\tauthors\tlink\tabstrct\n")
+    for i, paper in enumerate(conference_list):
+        outfile.write(f"{parse_paper_info(paper)}\n")
+
+#conference_list = parse_eccv(eccv_url, 1)
+#with open(f'./docs/ECCV2020.tsv', 'w') as outfile:
+#    for i, paper in enumerate(conference_list):
+#        outfile.write(f"{parse_paper_info(paper)}\n")
+#
+#conference_list = parse_eccv(eccv_url, 2)
+#with open(f'./docs/ECCV2018.tsv', 'w') as outfile:
+#    for i, paper in enumerate(conference_list):
+#        outfile.write(f"{parse_paper_info(paper)}\n")
+
+#conference_list = "CVPR2018,CVPR2019,ICCV2019,CVPR2020,CVPR2021"
+#for conference in "ICCV2021".split(","):
+#    conference_index = f"https://openaccess.thecvf.com/{conference}"
+#    conference_list = get_cvf_paper_list(conference_index)
+#
+#    with open(f'../docs/{conference}.tsv', 'w') as outfile:
+#        for i, paper in enumerate(conference_list):
+#            outfile.write(f"{parse_paper_info(paper)}\n")
