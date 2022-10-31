12ac0e90b9d56afd32152f832cced87c97db8bd9
jojolno <jojolno@outlook.com>
2022-10-31 23:32:36 +0800

    add script for quick filter by topic

---
+++ scripts/filter_by_topic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+topic=${1}
+conf=${2}
+
+data="./data/${conf}.tsv"
+./auto_read.py "Is this passage about $topic" ${data} tmp
+paste <(cat tmp | tr A-Z a-z) ${data} | awk '$1=="yes"' | cut -f2- > "${conf}_${topic}.tsv"
+rm tmp
